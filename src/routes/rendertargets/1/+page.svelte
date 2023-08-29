09aba73546b9a280791c91b83331b11d3e4932d9
jpaquim <joaopaquim@gmail.com>
2023-08-29 16:31:22 +0100

    Add examples for rendertargets 1

---
+++ src/routes/rendertargets/1/+page.svelte
@@ -0,0 +1,136 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import * as THREE from 'three';
+
+	let canvas: HTMLCanvasElement;
+
+	function main() {
+		const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
+
+		const rtWidth = 512;
+		const rtHeight = 512;
+		const renderTarget = new THREE.WebGLRenderTarget(rtWidth, rtHeight);
+
+		const rtFov = 75;
+		const rtAspect = rtWidth / rtHeight;
+		const rtNear = 0.1;
+		const rtFar = 5;
+		const rtCamera = new THREE.PerspectiveCamera(rtFov, rtAspect, rtNear, rtFar);
+		rtCamera.position.z = 2;
+
+		const rtScene = new THREE.Scene();
+		rtScene.background = new THREE.Color('red');
+
+		{
+			const color = 0xffffff;
+			const intensity = 3;
+			const light = new THREE.DirectionalLight(color, intensity);
+			light.position.set(-1, 2, 4);
+			rtScene.add(light);
+		}
+
+		const boxWidth = 1;
+		const boxHeight = 1;
+		const boxDepth = 1;
+		const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+
+		function makeInstance(
+			geometry: THREE.BufferGeometry,
+			color: THREE.ColorRepresentation,
+			x: number
+		): THREE.Mesh<THREE.BufferGeometry, THREE.MeshPhongMaterial> {
+			const material = new THREE.MeshPhongMaterial({ color });
+
+			const cube = new THREE.Mesh(geometry, material);
+			rtScene.add(cube);
+
+			cube.position.x = x;
+			return cube;
+		}
+
+		const rtCubes = [
+			makeInstance(geometry, 0x44aa88, 0),
+			makeInstance(geometry, 0x8844aa, -2),
+			makeInstance(geometry, 0xaa8844, 2)
+		];
+
+		const fov = 75;
+		const aspect = 2;
+		const near = 0.1;
+		const far = 5;
+		const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+		camera.position.z = 2;
+
+		const scene = new THREE.Scene();
+
+		{
+			const color = 0xffffff;
+			const intensity = 1;
+			const light = new THREE.DirectionalLight(color, intensity);
+			light.position.set(-1, 2, 4);
+			scene.add(light);
+		}
+
+		const material = new THREE.MeshPhongMaterial({
+			map: renderTarget.texture
+		});
+		const cube = new THREE.Mesh(geometry, material);
+		scene.add(cube);
+
+		function resizeRendererToDisplaySize(renderer: THREE.Renderer) {
+			const width = canvas.clientWidth;
+			const height = canvas.clientWidth;
+			const needResize = canvas.width !== width || canvas.height !== height;
+			if (needResize) {
+				renderer.setSize(width, height, false);
+			}
+			return needResize;
+		}
+
+		requestAnimationFrame(function render(time) {
+			time *= 0.001;
+
+			if (resizeRendererToDisplaySize(renderer)) {
+				camera.aspect = canvas.clientWidth / canvas.clientHeight;
+				camera.updateProjectionMatrix();
+			}
+
+			rtCubes.forEach((cube, ndx) => {
+				const speed = 1 + ndx * 0.1;
+				const rot = time * speed;
+				cube.rotation.x = rot;
+				cube.rotation.y = rot;
+			});
+
+			renderer.setRenderTarget(renderTarget);
+			renderer.render(rtScene, rtCamera);
+			renderer.setRenderTarget(null);
+
+			cube.rotation.x = time;
+			cube.rotation.y = time * 1.1;
+
+			renderer.render(scene, camera);
+
+			requestAnimationFrame(render);
+		});
+	}
+
+	onMount(() => {
+		main();
+	});
+</script>
+
+<canvas bind:this={canvas} />
+
+<style>
+	:global(html, body) {
+		margin: 0;
+		height: 100%;
+	}
+
+	canvas {
+		width: 100%;
+		height: 100%;
+		display: block;
+	}
+</style>
